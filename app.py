3878636a905400eea89fb786302dbe3326d7bb7d
menyafesto <146776954+menyafesto@users.noreply.github.com>
2025-06-08 12:56:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-import os
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Merhaba Render!"
+def login():
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
