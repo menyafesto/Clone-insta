f8df9059cd0d8cf6b43359bf0dc7a1341e147e80
menyafesto <146776954+menyafesto@users.noreply.github.com>
2025-06-08 12:49:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, render_template
 
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
